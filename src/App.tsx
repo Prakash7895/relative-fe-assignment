cd25991119ad3a11a6c219f8a60fdc5ebb05b7b4
Prakash Choudhary <prakashsaran0141@gmail.com>
2022-08-04 20:24:13 +0530

    ui complete

--- src/App.tsx
+++ src/App.tsx
@@ -2,98 +2,125 @@ import { Images } from './assets/Images';
 import TrendingCard from './components/TrendingCard';
 
 function App() {
-  const trendingArr = [
-    {
-      img: Images.Bitcoin,
-      title: 'Bitcoin (BTC)',
-      price: '$31,812.80',
-      change: { up: true, value: '10%' },
-      tvl: '$60,000',
-      popularPairs: [Images.Solana, Images.Ethereum, Images.Binance],
-    },
-    {
-      img: Images.Solana,
-      title: 'Solana (SOL)',
-      price: '$32.83',
-      change: { up: false, value: '12.32%' },
-      tvl: '$60,000',
-      popularPairs: [Images.Bitcoin, Images.Ethereum, Images.Binance],
-    },
-    {
-      img: Images.Ethereum,
-      title: 'Ethereum (ETH)',
-      price: '$1,466.45',
-      change: { up: false, value: '11.93%' },
-      tvl: '$60,000',
-      popularPairs: [Images.Solana, Images.Bitcoin, Images.Binance],
-    },
-    {
-      img: Images.Binance,
-      title: 'Binance USD (BUSD)',
-      price: '$1.00',
-      change: { up: true, value: '0.26%' },
-      tvl: '$60,000',
-      popularPairs: [Images.Solana, Images.Ethereum, Images.Binance],
-    },
-    {
-      img: Images.ShibaInu,
-      title: 'Shiba Inu (SHIB)',
-      price: '$0.00000001948',
-      change: { up: false, value: '8.1%' },
-      tvl: '$60,000',
-      popularPairs: [Images.Solana, Images.Ethereum, Images.Binance],
-    },
-  ];
+	const trendingArr = [
+		{
+			img: Images.Bitcoin,
+			imgContainerGradient: {
+				from: '#626a881a',
+				to: '#eab3000f',
+			},
+			title: 'Bitcoin (BTC)',
+			price: '$31,812.80',
+			change: { up: true, value: '10%' },
+			tvl: '$60,000',
+			popularPairs: [Images.Solana, Images.Ethereum, Images.Binance],
+		},
+		{
+			img: Images.Solana,
+			imgContainerGradient: {
+				from: '#626a881a',
+				to: '#dc1fff1a',
+			},
+			title: 'Solana (SOL)',
+			price: '$32.83',
+			change: { up: false, value: '12.32%' },
+			tvl: '$60,000',
+			popularPairs: [Images.Bitcoin, Images.Ethereum, Images.Binance],
+		},
+		{
+			img: Images.Ethereum,
+			imgContainerGradient: {
+				from: '#626a881a',
+				to: '#edf0f41a',
+			},
+			title: 'Ethereum (ETH)',
+			price: '$1,466.45',
+			change: { up: false, value: '11.93%' },
+			tvl: '$60,000',
+			popularPairs: [Images.Solana, Images.Bitcoin, Images.Binance],
+		},
+		{
+			img: Images.Binance,
+			imgContainerGradient: {
+				from: '#626a881a',
+				to: '#eab3000f',
+			},
+			title: 'Binance USD (BUSD)',
+			price: '$1.00',
+			change: { up: true, value: '0.26%' },
+			tvl: '$60,000',
+			popularPairs: [Images.Solana, Images.Ethereum, Images.Binance],
+		},
+		{
+			img: Images.ShibaInu,
+			imgContainerGradient: {
+				from: '#626a881a',
+				to: '#e42d041a',
+			},
+			title: 'Shiba Inu (SHIB)',
+			price: '$0.00000001948',
+			change: { up: false, value: '8.1%' },
+			tvl: '$60,000',
+			popularPairs: [Images.Solana, Images.Ethereum, Images.Binance],
+		},
+	];
 
-  return (
-    <div className='h-screen flex flex-col items-center mx-10'>
-      <div className='border border-red-500 w-full mb-10'>
-        <img
-          src={Images.Activity}
-          alt='Activity'
-          className='mr-4 inline-block'
-        />
-        <span className='leading-8 tracking-[-0.01em]'>Trending Assets</span>
-      </div>
-      <div className='border p-0 border-red-500 w-full grid md:grid-cols-3 sm:grid-cols-2 gap-8'>
-        {trendingArr.map((el) => (
-          <TrendingCard headerElement={<img src={el.img} alt={el.title} />}>
-            <div>
-              <p className='text-[#737BAE]'>{el.title}</p>
-              <p className='bg-[#14172B] rounded-2xl mt-4 mb-2 p-3 relative'>
-                {el.price}
-                <span
-                  className={`absolute right-3 ${
-                    el.change.up ? 'text-[#00FFA3]' : 'text-[#FF4D4D]'
-                  }`}
-                >
-                  {(el.change.up ? '+' : '-') + el.change.value}
-                </span>
-              </p>
-              <p className='text-[#737BAE]'>Price</p>
-              <p className='bg-[#14172B] rounded-2xl mt-4 mb-2 p-3'>{el.tvl}</p>
-              <p className='text-[#737BAE]'>TVL</p>
-              <div className='flex justify-center  mt-4 mb-2'>
-                <div className='flex bg-[#14172B] rounded-2xl p-3'>
-                  {el.popularPairs.map((p) => (
-                    <div>
-                      <img
-                        width={'60%'}
-                        className='mx-auto'
-                        src={p}
-                        alt='popular Pairs'
-                      />
-                    </div>
-                  ))}
-                </div>
-              </div>
-              <p className='text-[#737BAE]'>Popular pairs</p>
-            </div>
-          </TrendingCard>
-        ))}
-      </div>
-    </div>
-  );
+	return (
+		<div className='min-h-screen overflow-auto flex justify-center items-center py-10'>
+			<div className='w-full flex flex-col items-center mx-10'>
+				<div className='w-full mb-10'>
+					<img
+						src={Images.Activity}
+						alt='Activity'
+						className='mr-4 inline-block'
+					/>
+					<span className='leading-8 tracking-[-0.01em]'>Trending Assets</span>
+				</div>
+				<div className='w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4 2xl:gap-16'>
+					{trendingArr.map((el) => (
+						<TrendingCard
+							headerElement={<img src={el.img} alt={el.title} />}
+							headerElementContainerStyle={`bg-gradient-to-b from-[${el.imgContainerGradient.from}] to-[${el.imgContainerGradient.to}] backdrop-blur-[100px]`}
+						>
+							<div>
+								<p className='text-[#737BAE]'>{el.title}</p>
+								<div className='bg-[#14172B] rounded-2xl mt-4 mb-2 p-3 flex justify-center'>
+									<p className='flex-1'>{el.price}</p>
+									<span
+										className={`${
+											el.change.up ? 'text-[#00FFA3]' : 'text-[#FF4D4D]'
+										}`}
+									>
+										{(el.change.up ? '+' : '-') + el.change.value}
+									</span>
+								</div>
+								<p className='text-[#737BAE]'>Price</p>
+								<p className='bg-[#14172B] rounded-2xl mt-4 mb-2 p-3'>
+									{el.tvl}
+								</p>
+								<p className='text-[#737BAE]'>TVL</p>
+								<div className='flex justify-center  mt-4 mb-2'>
+									<div className='flex bg-[#14172B] rounded-2xl p-3'>
+										{el.popularPairs.map((p) => (
+											<div>
+												<img
+													width={'50%'}
+													className='mx-auto'
+													src={p}
+													alt='popular Pairs'
+												/>
+											</div>
+										))}
+									</div>
+								</div>
+								<p className='text-[#737BAE]'>Popular pairs</p>
+							</div>
+						</TrendingCard>
+					))}
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
--- src/components/TrendingCard.tsx
+++ src/components/TrendingCard.tsx
@@ -1,84 +1,89 @@
 import { ReactNode } from 'react';
 
 interface TrendingCardProps {
-  borderColor?: string;
-  gradientFromColor?: string;
-  gradientToColor?: string;
-  headerElement: ReactNode;
-  children: ReactNode;
+	borderColor?: string;
+	gradientFromColor?: string;
+	gradientToColor?: string;
+	headerElement: ReactNode;
+	children: ReactNode;
+	className?: string;
+	headerElementContainerStyle?: string;
 }
 
 const TrendingCard: React.FC<TrendingCardProps> = ({
-  borderColor,
-  gradientFromColor,
-  gradientToColor,
-  headerElement,
-  children,
+	borderColor,
+	gradientFromColor,
+	gradientToColor,
+	headerElement,
+	children,
+	className,
+	headerElementContainerStyle,
 }) => {
-  const sideCurves = 'border-t flex-1 h-20 rounded-t-2xl';
+	const sideCurves = 'border-t flex-1 h-20 rounded-t-2xl';
 
-  const backgroundStyle = {
-    borderColor: borderColor,
-    background: `linear-gradient(180deg, ${gradientFromColor} 0%, ${gradientToColor} 107.43%)`,
-    backdropFilter: 'blur(100px)',
-  };
+	const backgroundStyle = {
+		borderColor: borderColor,
+		background: `linear-gradient(180deg, ${gradientFromColor} 0%, ${gradientToColor} 107.43%)`,
+		backdropFilter: 'blur(100px)',
+	};
 
-  return (
-    <div className={`flex flex-col items-center`}>
-      <div className='flex flex-row w-full items-end'>
-        <div
-          className={`${sideCurves} border-l rounded-tr-[35px]`}
-          style={{
-            backgroundColor: gradientFromColor,
-            borderColor: borderColor,
-          }}
-        ></div>
-        <div className='h-28 w-28 relative'>
-          <div
-            className='h-11 absolute bottom-0 w-full z-0'
-            style={{
-              background: gradientFromColor,
-            }}
-          ></div>
-          <div
-            className='border-b rounded-full h-full w-full relative z-10 p-3 ring-bw-head-body'
-            style={{
-              borderColor: borderColor,
-            }}
-          >
-            <div
-              className='border rounded-full h-full w-full flex justify-center items-center'
-              style={{
-                // background: gradientFromColor,
-                borderColor: borderColor,
-              }}
-            >
-              {headerElement}
-            </div>
-          </div>
-        </div>
-        <div
-          className={`${sideCurves} border-r rounded-tl-[35px]`}
-          style={{
-            background: gradientFromColor,
-            borderColor: borderColor,
-          }}
-        ></div>
-      </div>
-      <div
-        className='border border-t-0 rounded-b-2xl w-full p-5 text-center'
-        style={{ ...backgroundStyle }}
-      >
-        {children}
-      </div>
-    </div>
-  );
+	return (
+		<div className={`flex flex-col items-center ${className}`}>
+			<div className='flex flex-row w-full items-end'>
+				<div
+					className={`${sideCurves} border-l rounded-tr-[35px]`}
+					style={{
+						backgroundColor: gradientFromColor,
+						borderColor: borderColor,
+					}}
+				></div>
+				<div className='h-28 w-28 relative'>
+					<div
+						className='h-11 absolute bottom-0 w-full z-0'
+						style={{
+							background: gradientFromColor,
+						}}
+					></div>
+					<div
+						className='border-b rounded-full h-full w-full relative z-10 p-3 ring-bw-head-body'
+						style={{
+							borderColor: borderColor,
+						}}
+					>
+						<div
+							className={`border rounded-full h-full w-full flex justify-center items-center ${headerElementContainerStyle}`}
+							style={{
+								// background: gradientFromColor,
+								borderColor: borderColor,
+							}}
+						>
+							{headerElement}
+						</div>
+					</div>
+				</div>
+				<div
+					className={`${sideCurves} border-r rounded-tl-[35px]`}
+					style={{
+						background: gradientFromColor,
+						borderColor: borderColor,
+					}}
+				></div>
+			</div>
+			<div
+				className='border border-t-0 rounded-b-2xl w-full p-5 text-center'
+				style={{ ...backgroundStyle }}
+			>
+				{children}
+			</div>
+		</div>
+	);
 };
 
 export default TrendingCard;
 
 TrendingCard.defaultProps = {
-  borderColor: '#ECF0FF1A',
-  gradientFromColor: 'rgba(98, 106, 136, 0.1)',
-  gradientToColor: 'rgba(46, 49, 73, 0.1)',
+	borderColor: '#ECF0FF1A',
+	gradientFromColor: 'rgba(98, 106, 136, 0.1)',
+	gradientToColor: 'rgba(46, 49, 73, 0.1)',
+	headerElementContainerStyle: 'bg-red-300',
 };
